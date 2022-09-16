69f494e6eab48f57e8ad8523f2d6f0afb5a3a885
opeyemiabdulsalam <opeyemiabdulsalam2442@gmail.com>
2022-09-16 09:42:56 -0400

    commit stringvar

---
+++ src/StringVar.java
@@ -0,0 +1,24 @@
+public class StringVar {
+    public static void main(String[] args) {
+        //Primitive data type
+        //Object data type
+
+//        int, double, float, byte, short, long
+//        String
+          String myAddress = "room 504 Teachers Dyers Complex";
+          String FIRST_NAME = "Opeyemi";
+          String lastName = "Abdulsalam";
+          String fullName = FIRST_NAME + " " + lastName;
+          System.out.println("Fullname is: " + fullName);
+
+
+          FIRST_NAME = "John";
+
+          int shoeSize = 10;
+          int clothSize = 6;
+
+        System.out.println( "fullName + shoeSize: " + fullName + shoeSize);
+        System.out.println( "shoeSize + clothSize: " + (shoeSize + clothSize));
+
+    }
+}
